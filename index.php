c113034b5d210af582de676a92bb1d5a78e8550c
Nenyi Keborku <nenyikeborku@Nenyis-MacBook-Pro.local>
2020-11-11 21:54:25 -0500

    tes php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php 
+
+include 'classes/Layout.php';
+
 // GET DATABASE
 require 'connect.php'; 
-include 'Layout.php'; 
+
 
 // Get all unique langs
 $sql = "SELECT DISTINCT countrylanguage.Language from countrylanguage";
