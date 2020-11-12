109243b58f93083c44a9272f24f94abad472a22e
Nenyi Keborku <nenyikeborku@Nenyis-MacBook-Pro.local>
2020-11-11 22:18:26 -0500

    add css

--- classes/Layout.php
+++ classes/Layout.php
@@ -19,6 +19,7 @@ class Layout {
         echo '  <head>';
         echo '      <title>Languages of the World</title>';
         echo '      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">';
+        echo '      <link rel="stylesheet" href="styles.css">';
         echo '  </head>';
     }
 
