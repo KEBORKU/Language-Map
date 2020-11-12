bf31879650da9a21659704ea0c10c04ecfb50ec8
Nenyi Keborku <nenyikeborku@Nenyis-MacBook-Pro.local>
2020-11-11 21:26:21 -0500

    made layout

--- index.php
+++ index.php
@@ -1,36 +1,25 @@
 <?php 
 // GET DATABASE
 require 'connect.php'; 
-?>
-<!doctype html>
-<html lang="en">
-    <head>
-        <title>Languages of the World</title>
-    </head>
-    <body>
-        <h1>Languages<h1>
+require 'layout.php';
 
-<?php
 
 // Get all unique langs
 $sql = "SELECT DISTINCT countrylanguage.Language from countrylanguage";
 $result = $db->query($sql);
 
+$items = array();
+
 if ($result->num_rows > 0) {
-    // for each lang it makes a link 
     while($row = $result->fetch_array()) {
         $name = $row["Language"];
-        echo
-        '<div class="country">'.
-            '<a href="countries.php?lang='.$name.'" class="name">'.$name.'</a>'.
-        '</div>';
+        array_push($items, $name);
     }
-} else {
-    echo "<p>Nothing to show</p>";
 }
 
 $db->close();
 
+$layout = Layout('Languages', $items);
+$layout.show();
+
 ?>
-    </body>
-</html>
