15504676506b0204fa9831e693da6715ff3e2f50
Nenyi Keborku <nenyikeborku@Nenyis-MacBook-Pro.local>
2020-11-11 22:04:09 -0500

    add list

--- index.php
+++ index.php
@@ -9,12 +9,12 @@ $result = $db->query($sql);
 
 $items = array();
 
-// if ($result->num_rows > 0) {
-//     while($row = $result->fetch_array()) {
-//         $name = $row["Language"];
-//         array_push($items, $name);
-//     }
-// }
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_array()) {
+        $name = $row["Language"];
+        array_push($items, $name);
+    }
+}
 
 $db->close();
 
